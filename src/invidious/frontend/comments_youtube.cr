f92d3262ad9eb8de477de77bfd4e11612b6ad58f
abb128 <abb128@sapples.net>
2024-09-25 21:38:44 +0300

    Update styling

--- src/invidious/frontend/comments_youtube.cr
+++ src/invidious/frontend/comments_youtube.cr
@@ -68,7 +68,7 @@ module Invidious::Frontend::Comments
         html << <<-END_HTML
         <div class="pure-g vidComment" style="width:100%">
           <div class="vidCommentImgDiv channel-profile pure-u-4-24 pure-u-md-2-24">
-            <img loading="lazy" style="margin-right:1em;margin-top:1em;width:90%" src="#{author_thumbnail}" alt="" />
+            <img loading="lazy" style="margin-right:1em;margin-top:0.1em;width:90%" src="#{author_thumbnail}" alt="" />
           </div>
           <div class="vidCommentDDiv pure-u-20-24 pure-u-md-22-24">
             <p>
